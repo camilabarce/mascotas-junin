408edc5a1e56b0b330139672e922ab09d251f52e
Camila Arce <arce.b.camila@gmail.com>
2024-06-05 20:00:09 -0300

    validacion form con formsubmit

--- js/form.js
+++ js/form.js
@@ -1,19 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const form = document.getElementById('form-perdidos');
-    const inputs = document.querySelectorAll('.input');
-    const fileInput = document.querySelector('.file-input');
+    const input = document.querySelector('.file-input');
     const label = document.querySelector('.file-label');
     const preview = document.getElementById('preview');
 
-    // Validate form inputs on change and input events
-    inputs.forEach(input => {
-        input.addEventListener('input', validateInput);
-    });
-
-    fileInput.addEventListener('change', function () {
-        validateFileInput();
-
-        const file = fileInput.files[0];
+    input.addEventListener('change', function () {
+        const file = input.files[0];
         if (file) {
             const fileName = file.name;
             label.textContent = fileName;
@@ -30,46 +21,4 @@ document.addEventListener('DOMContentLoaded', function () {
             preview.src = '';
         }
     });
-
-    form.addEventListener('submit', function (event) {
-        let valid = true;
-        inputs.forEach(input => {
-            if (!validateInput({ target: input })) {
-                valid = false;
-            }
-        });
-
-        if (!validateFileInput()) {
-            valid = false;
-        }
-
-        if (!valid) {
-            event.preventDefault();
-        }
-    });
-
-    function validateInput(event) {
-        const input = event.target;
-        if (input.checkValidity()) {
-            input.classList.add('valid');
-            input.classList.remove('invalid');
-            return true;
-        } else {
-            input.classList.add('invalid');
-            input.classList.remove('valid');
-            return false;
-        }
-    }
-
-    function validateFileInput() {
-        if (fileInput.files.length > 0) {
-            fileInput.classList.add('valid');
-            fileInput.classList.remove('invalid');
-            return true;
-        } else {
-            fileInput.classList.add('invalid');
-            fileInput.classList.remove('valid');
-            return false;
-        }
-    }
 });
