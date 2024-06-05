16d473effae0ede772dd665e6994515252351ee5
Camila Arce <arce.b.camila@gmail.com>
2024-06-05 14:56:39 -0300

    modal para detalle de mascotas perdidas

--- js/perdidos.js
+++ js/perdidos.js
@@ -13,14 +13,30 @@ function loadLostPetsList(dataFile) {
                 const pet = data[id];
                 const petItem = `
                     <li class="cards-item perdidos">
-                        <img src="${pet.img}" alt="Imagen de ${pet.nombre}">
+                        <a href="#open-modal" class="open-modal" data-id="${id}"><img src="${pet.img}" alt="Imagen de ${pet.nombre}"></a>
                         <div class="text-card-container">
                             <h3>${pet.nombre}</h3>
-                            <p>Perdido el ${pet.fecha}</p>
+                            <p>Se perdió el ${pet.fecha}</p>
                         </div>
                     </li>`;
                 container.insertAdjacentHTML('beforeend', petItem);
             }
+
+            //event listener al modal
+            const modalLinks = document.querySelectorAll('.open-modal');
+            modalLinks.forEach(link => {
+                link.addEventListener('click', function (event) {
+                    const petId = this.getAttribute('data-id');
+                    openModal(data[petId]);
+                });
+            });
         })
-        .catch(error => console.error(`Error al cargar la lista de mascotas perdidas:`, error));
+        .catch(error => console.error('Error al cargar la lista de mascotas perdidas:', error));
+}
+
+function openModal(petData) {
+    document.getElementById('modal-nombre').innerText = petData.nombre;
+    document.getElementById('modal-descripcion').innerText = petData.descripcion;
+    document.getElementById('modal-fecha-barrio').innerText = `Fue visto por última vez el ${petData.fecha} en ${petData.barrio}`;
+    document.getElementById('modal-contacto').innerText = `Si lo viste, avisá al ${petData.contacto} - ${petData['nombre-dueno']}`;
 }
